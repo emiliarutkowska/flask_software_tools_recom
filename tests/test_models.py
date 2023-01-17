b4e4addddf9a6c600b666fbc334a66b2c9064ba1
emiliarutkowska <emilia.weronika.rutkowska@gmail.com>
2023-01-17 20:52:11 +0100

    Add test

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,4 @@
-from models.models import Tool
+from models.models import Tool, Review
 
 
 def test_new_software_tool():
@@ -11,3 +11,16 @@ def test_new_software_tool():
     assert tool.name == "Newtool"
     assert tool.link == "https://newtool.com"
     assert tool.description == "Decritpion"
+
+
+def test_new_review():
+    """
+    GIVEN a Review model
+    WHEN a new Review is created
+    THEN check the user_name, rating, and review_test fields are defined correctly
+    """
+    new_review = Review("3", "John", 4, "Very nice tool")
+    assert new_review.user_name == "John"
+    assert new_review.rating == 4
+    assert new_review.review_text == "Very nice tool"
+
